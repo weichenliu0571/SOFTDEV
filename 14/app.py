f27ce976dd84086b9ea88af969a151a353db4c9f
Weichen <wliu30@stuy.edu>
2022-10-19 09:21:01 -0400

    a

---
+++ 14/app.py
@@ -0,0 +1,19 @@
+'''
+
+SoftDev
+K10
+2022-10-13
+Time Spent: .5 hrs
+'''
+
+from flask import Flask, render_template #Q0: What will happen if you remove render_template from this line? (log your prediction before you pull the trigger...)
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
