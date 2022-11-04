cf2cfdbfd549a8af123cc912525a4874ed38e27b
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2022-11-03 23:01:49 -0400

    Update app.py

--- 19_session/app.py
+++ 19_session/app.py
@@ -1,45 +1,56 @@
-# AEIOU : BRIAN, WEICHEN, VANSH
+# Brian, Vansh, Weichen
 # SoftDev
-# K19 -- SESSIONS
-# 2022-11-03
-# time spent : 0.5 hrs
+# K19 -- Sessions Greetings
+# 2022-11-03 
+# time spent: 30 mins
 
-from flask import Flask, redirect, url_for, render_template, request, session
 
-app = Flask(__name__)
+from flask import Flask, redirect, url_for             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+from flask import session
 
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-users = {'m':'donky'} # hardcode users
 
-@app.route("/", methods = ["POST", "GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["username"]
-        password = request.form.get['password']
-        if user not in users.keys():
-            return redirect(url_for("login"), error = "WRONG USERNAME OR WRONG PASSWORD")
-        if password != users(user):
-            return redirect(url_for("login"), error = "WRONG PASSWORD" )
+users = {'m':'donky'}
+
+
+@app.route("/") #, methods=['GET', 'POST'])
+def index():
+    if 'username' in session:
+        return render_template('response.html',
+        user=session["username"])
+    return render_template( 'login.html',)
+
+
+@app.route("/auth") # , methods=[ 'POST'])
+def authenticate():
+    if request.method == 'GET':
+        user = request.args.get('username')
+        password = request.args.get('password')
+        if user not in users.keys(): #check if user exists
+            error = "User DNE"
+            return render_template('login.html',
+            error=error)
+        if password != users[user]: #check if password matches user
+            error = "Wrong Password"
+            return render_template('login.html',
+            error=error)
         session["username"] = user
-        return redirect(url_for("user"))
-    else:
-        if "user" in session:
-            return redirect(url_for("user"))
-        return render_template("login.html")
+        return redirect(url_for('index'))  #redirects to home page
 
-@app.route("/user")
-def user():
-    if "username" in session:
-        user = session["username"]
-        return render_template("response.html", name = user)
-    else:
-        return redirect(url_for("user"))
 
 @app.route("/logout")
 def logout():
-    session.pop("user", None)
-    return redirect(url_for("login"))
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
