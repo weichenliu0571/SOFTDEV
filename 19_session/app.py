341bc6f9dab4cb921e73a249a01e6d4df5c4d158
Weichen <wliu30@stuy.edu>
2022-11-03 20:33:57 -0400

    added stuff

--- 19_session/app.py
+++ 19_session/app.py
@@ -6,31 +6,34 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 users = {'m':'donky'} # hardcode users
 
-@app.route("/")
-def disp_loginpage():
-    if 'username' in session:
-        return render_template('response.html',
-        user = session["username"])
-    return render_template('login.html')
-
-@app.route("/auth")
-def authenticate():
-    if request.method == 'POST':
-        user = request.form.get['username']
+@app.route("/", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["username"]
         password = request.form.get['password']
         if user not in users.keys():
-            error = "USER DNE"
-            return render_template('login.html', error = error)
+            return redirect(url_for("login"), error = "WRONG USERNAME OR WRONG PASSWORD")
         if password != users(user):
-            error = "Wrong Password"
-            return render_template('login.html', error = error)
-        
+            return redirect(url_for("login"), error = "WRONG PASSWORD" )
         session["username"] = user
-        return redirect(url_for('index'))
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "username" in session:
+        user = session["username"]
+        return render_template("response.html", name = user)
+    else:
+        return redirect(url_for("user"))
 
 @app.route("/logout")
 def logout():
-    return render_template( 'login.html' ) #brings back to homepage
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
