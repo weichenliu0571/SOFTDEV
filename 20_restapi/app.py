334881964d13fc78f03d2a8a9ac0275e8cdb79d1
Weichen <wliu30@stuy.edu>
2022-11-21 20:10:12 -0500

    a

---
+++ 20_restapi/app.py
@@ -0,0 +1,31 @@
+# Balls (Weichen + Jeffrey)
+# SoftDev
+# K20: API STUFF
+# 2022-11-21
+# time spent: .5 hrs
+
+from flask import Flask, escape, request, render_template
+import urllib3
+import json
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    with open('key_nasa.txt') as f:
+        apikey = f.read() # retrieves API key
+
+    http = urllib3.PoolManager()
+    link = 'https://api.nasa.gov/planetary/apod?api_key=' + apikey
+
+    response = http.request('GET', link)
+    _data = response.data
+
+    print(_data)
+
+    image = json.loads(_data)["url"]
+
+    return render_template("main.html", image = image)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
