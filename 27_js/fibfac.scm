d7f04df9b972bfbc2e75403095e95be2a3e211a3
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2023-04-04 13:38:23 -0400

    Create fibfac.scm

---
+++ 27_js/fibfac.scm
@@ -0,0 +1,27 @@
+;Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon
+;SoftDev pd0
+;K27 -- Basic functions in JavaScript
+;2023-04-03t
+
+;Scheme Antecedents for JS work
+
+;factorial:
+;<your team's fact(n) implementation>
+
+;(fact 1) ;"...should be  1"
+;(fact 2) ;"...should be  2"
+;(fact 3) ;"...should be  6"
+;(fact 4) ;"...should be  24"
+;(fact 5) ;"...should be  120"
+
+
+;fib:
+;<your team's fib(n) implementation>
+
+;(fib 0) ;"...should be  0"
+;(fib 1) ;"...should be  1"
+;(fib 2) ;"...should be  1"
+;(fib 3) ;"...should be  2"
+;(fib 4) ;"...should be  3"
+;=================================================================
+
