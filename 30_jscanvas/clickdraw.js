21a409aa5dc7183530b9b31559279d72e5060fd1
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2023-04-25 13:28:44 -0400

    Update clickdraw.js

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -1,3 +1,11 @@
+/*
+Keychron K23: Jeff Chen, Weichen Liu
+SoftDev pd8
+K30 -- basic canvas work in JS
+2023-04-25t
+time spent: 
+*/
+
 //retrieve node in DOM via ID
 var c = document.getElementById("slate");
 
@@ -7,48 +15,54 @@ var ctx = c.getContext("2d");
 //init globar state var
 var mode = "rect";
 
+// set shape color
+ctx.fillStyle = "red";
+ctx.strokeStyle = "red";
+
 //var toggleMode = function(e){
 var toggleMode = (e) => {
     console.log("toggling...");
     if (mode === "rect") {
         mode = "circ";
-    } else {
+    }
+    else {
         mode = "rect";
     }
-}
+};
 
 var drawRect = (e) => {
     var mouseX = e.offsetX; //gets X-coor of mouse when event is fired
     var mouseY = e.offsetY; //gets Y-coor of mouse when event is fired
     console.log("mouseclick registered at ", mouseX, mouseY);
-    ctx.fillRect(mouseX, mouseY, 15, 20);
-}
+    ctx.fillRect(mouseX, mouseY, 80, 100);
+};
 
-var drawCricle = (e) => {
-    var mouseX = e.offsetX;
-    var mouseY = e.offsetY;
+var drawCircle = (e) => {
+    var mouseX = e.offsetX; //gets X-coor of mouse when event is fired
+    var mouseY = e.offsetY; //gets Y-coor of mouse when event is fired
     console.log("mouseclick registered at ", mouseX, mouseY);
     ctx.beginPath();
-    ctx.arc(mouseX, mouseY, 10, 0, 2*Math.PI);
+    ctx.arc(mouseX, mouseY, 50, 0, Math.PI * 2);
     ctx.stroke();
     ctx.fill();
-}
+};
 
 var draw = (e) => {
     if (mode === "rect") {
         drawRect(e);
     } else {
-        drawCricle(e);
+        drawCircle(e);
     }
-}
+};
 
 var wipeCanvas = () => {
+    ctx.clearRect(0, 0, c.width, c.height);
+};
 
-}
+c.addEventListener("click", draw); //passes the mouse event as parameter for the function
 
-
-c.addEventListener("click", draw);
 var bToggler = document.getElementById("buttonToggle");
 bToggler.addEventListener("click", toggleMode);
+
 var clearB = document.getElementById("buttonClear");
-clearB.addEventListener("click", wipeCanvas)
+clearB.addEventListener("click", wipeCanvas);
