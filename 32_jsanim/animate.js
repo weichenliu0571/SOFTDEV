51ccfb38a95aa8dad0d204983fca84692143e409
Weichen <wliu30@stuy.edu>
2023-04-26 14:07:10 -0400

    added 32

--- 32_jsanim/animate.js
+++ 32_jsanim/animate.js
@@ -1,6 +1,7 @@
 // Get elements for Cavas Dot Button and Stop Button
 var c = document.getElementById("playground");
 var dotButton = document.getElementById("buttonCircle");
+var dvdButton = document.getElementById("dvd");
 var stopButton = document.getElementById("buttonStop");
 
 var ctx = c.getContext("2d");
@@ -10,12 +11,44 @@ ctx.strokeStyle = "black";
 var requestID; // init global var for use with animation frames
 
 var clear = (e) => {
+    e.preventDefault(); // 
     ctx.clearRect(0, 0, c.width, c.height);
 };
 
 var radius = 100;
 var growing = true;
 
+var dvdLogoSetup = function() {
+    window.cancelAnimationFrame( requestID );
+    var rectWidth = 600;
+    var rectHeight = 400;
+    
+    var rectX = Math.random() * 500;
+    var recty = Math.random() * 500;
+
+    var xVel = 10;
+    var yVel = 10;
+
+    var logo = new Image();
+    logo.src = "logo_dvd.jpg";
+
+    var dvdLogo = function() {
+        ctx.clearRect(0,0,c.width,c.height);
+        //ctx.fillRect(rectx, rectY, rectWidth, rectHeight);
+        ctx.drawImage(logo, rectX, rectY, rectWidth, rectHeight);
+        if (rectX = 500) {
+            xVel = xVel * -1;
+        }
+        if (rectY = 500) {
+            yVel = yVel * -1;
+        }
+        rectX = rectX + xVel;
+        rectY = rectX + yVel;
+        window.cancelAnimationFrame(requestID);
+        requestID = window.requestAnimationFrame(dvdLogo);
+        };
+    dvdLogo();
+}
 
 // // drawCircle command taken from k30 with added parameter r
 // var drawCircle = (e, r) => {
@@ -70,4 +103,5 @@ var stopIt = () => {
 };
 
 dotButton.addEventListener("click", drawDot);
+dvdButton.addEventListener("click", dvdLogoSetup)
 stopButton.addEventListener("click", stopIt);
