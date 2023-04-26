fa61ddf4fa47ea575b134c5449463407bbe2ad51
Weichen Liu <73133273+weichenliu0571@users.noreply.github.com>
2023-04-25 23:06:27 -0400

    Update animate.js

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -1,62 +1,54 @@
-// Get elements for Cavas Dot Button and Stop Button
+/*
+Nakib Abedin, Weichen Liu
+SoftDev pd8
+K31 -- animation in canvas JS
+2023-04-26w
+*/
+
+
 var c = document.getElementById("playground");
 var dotButton = document.getElementById("buttonCircle");
-var stopButton = document.getElementById("buttonStop");
+var  stopButton = document.getElementById("buttonStop");
 
 var ctx = c.getContext("2d");
-ctx.fillStyle = "blue";
 
-var requestID; // init global var for use with animation frames
+ctx.fillStyle = "#ADD8E6";
 
-var clear = (e) => {
+var requestID;
+
+var clear = () => {
     ctx.clearRect(0, 0, c.width, c.height);
-};
+}
 
-var radius = 0;
+var radius = 100;
 var growing = true;
 
-
-// drawCircle command taken from k30 with added parameter r
-var drawCircle = (e, r) => {
-    var mouseX = e.offsetX; //gets X-coor of mouse when event is fired
-    var mouseY = e.offsetY; //gets Y-coor of mouse when event is fired
-    console.log("mouseclick registered at ", mouseX, mouseY);
-    ctx.beginPath();
-    ctx.arc(mouseX, mouseY, r, 0, Math.PI * 2);
-    ctx.stroke();
-    ctx.fill();
-};
-
-
-var drawDot = () => {
-    /*
-    Wipe the canvas,
-    repaint the circle,
-
-    ...and somewhere (where/when is the right time?)
-    Update requestID to propagate the animation
-    You will need 
-    window.cancelAnimationFrame()
-    window.requestAnimationFrame()
-    */
-    ctx.clearRect(0, 0, c.width, c.height);
-    drawCircle(e, radius);
-    radius++;
-    requestID = window.requestAnimationFrame();
-
+var drawDot = () =>{
+    clear();
+    if(radius === c.height / 2) {growing = false}
+    if(radius === 0) {growing = true}
+        if (growing){
+            radius++;
+        }
+        else{
+            radius--;
+        }
+        console.log(radius);
+        ctx.beginPath();
+        ctx.arc(c.width / 2, c.height / 2, radius, 0, Math.PI * 2);
+        ctx.stroke();
+        ctx.fill();
+        window.cancelAnimationFrame(requestID);
+        requestID = window.requestAnimationFrame(drawDot);
 }
 
 var stopIt = () => {
-    console.log("stopIt invoked");
-    console.log( requestID );
-
+    console.log("stopit invoked...");
     window.cancelAnimationFrame(requestID);
-    /*
-    YOUR CODE HERE
-    ... to stop the animation
-    you will need window.cancelAnimationFrame()
-    */
-};
+    console.log("requestID cancelled");
+
+}
+
 
 dotButton.addEventListener("click", drawDot);
 stopButton.addEventListener("click", stopIt);
